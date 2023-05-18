df2797b4b612e061c7a60b38a92699e55d624ede
Crowdbotics <team@crowdbotics.com>
2023-05-18 20:27:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "alexion-query-tool-40829",
+  url: "https://alexion-query-tool-40829.botics.co",
+  api: "https://alexion-query-tool-40829.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
